9d7d47581be3ae4edc21cc7b9cd2132525c8c075
Bryce Dowhower <bryce.dowhower@ou.edu>
2019-02-19 10:36:40 -0600

    Add project files.

---
+++ ETL/frmClassAnalytics.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace ETL
+{
+    partial class frmClassAnalytics
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtBxSelectFolder = new System.Windows.Forms.TextBox();
+            this.BtnSelectDirectory = new System.Windows.Forms.Button();
+            this.BtnRunAnalysis = new System.Windows.Forms.Button();
+            this.ListBox1 = new System.Windows.Forms.ListBox();
+            this.Groupbox1 = new System.Windows.Forms.GroupBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.BtnPopFiles = new System.Windows.Forms.Button();
+            this.Groupbox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TxtBxSelectFolder
+            // 
+            this.TxtBxSelectFolder.Location = new System.Drawing.Point(53, 76);
+            this.TxtBxSelectFolder.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.TxtBxSelectFolder.Name = "TxtBxSelectFolder";
+            this.TxtBxSelectFolder.Size = new System.Drawing.Size(865, 38);
+            this.TxtBxSelectFolder.TabIndex = 1;
+            // 
+            // BtnSelectDirectory
+            // 
+            this.BtnSelectDirectory.Location = new System.Drawing.Point(1003, 45);
+            this.BtnSelectDirectory.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.BtnSelectDirectory.Name = "BtnSelectDirectory";
+            this.BtnSelectDirectory.Size = new System.Drawing.Size(333, 148);
+            this.BtnSelectDirectory.TabIndex = 0;
+            this.BtnSelectDirectory.Text = "Select Directory";
+            this.BtnSelectDirectory.UseVisualStyleBackColor = true;
+            this.BtnSelectDirectory.Click += new System.EventHandler(this.BtnSelectDirectory_Click);
+            // 
+            // BtnRunAnalysis
+            // 
+            this.BtnRunAnalysis.Location = new System.Drawing.Point(1086, 1008);
+            this.BtnRunAnalysis.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.BtnRunAnalysis.Name = "BtnRunAnalysis";
+            this.BtnRunAnalysis.Size = new System.Drawing.Size(323, 114);
+            this.BtnRunAnalysis.TabIndex = 8;
+            this.BtnRunAnalysis.Text = "Run Analysis";
+            this.BtnRunAnalysis.UseVisualStyleBackColor = true;
+            this.BtnRunAnalysis.Click += new System.EventHandler(this.BtnRunAnalysis_Click);
+            // 
+            // ListBox1
+            // 
+            this.ListBox1.FormattingEnabled = true;
+            this.ListBox1.ItemHeight = 31;
+            this.ListBox1.Location = new System.Drawing.Point(83, 527);
+            this.ListBox1.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.ListBox1.Name = "ListBox1";
+            this.ListBox1.Size = new System.Drawing.Size(1345, 438);
+            this.ListBox1.TabIndex = 5;
+            // 
+            // Groupbox1
+            // 
+            this.Groupbox1.Controls.Add(this.TxtBxSelectFolder);
+            this.Groupbox1.Controls.Add(this.BtnSelectDirectory);
+            this.Groupbox1.Location = new System.Drawing.Point(83, 193);
+            this.Groupbox1.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.Groupbox1.Name = "Groupbox1";
+            this.Groupbox1.Padding = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.Groupbox1.Size = new System.Drawing.Size(1352, 238);
+            this.Groupbox1.TabIndex = 7;
+            this.Groupbox1.TabStop = false;
+            this.Groupbox1.Text = "Select a Folder that has all of your files";
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(397, 43);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(649, 98);
+            this.lblTitle.TabIndex = 6;
+            this.lblTitle.Text = "Class Analytics";
+            // 
+            // BtnPopFiles
+            // 
+            this.BtnPopFiles.Location = new System.Drawing.Point(136, 1018);
+            this.BtnPopFiles.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.BtnPopFiles.Name = "BtnPopFiles";
+            this.BtnPopFiles.Size = new System.Drawing.Size(323, 114);
+            this.BtnPopFiles.TabIndex = 9;
+            this.BtnPopFiles.Text = "Populate Files";
+            this.BtnPopFiles.UseVisualStyleBackColor = true;
+            this.BtnPopFiles.Click += new System.EventHandler(this.BtnPopFiles_Click);
+            // 
+            // frmClassAnalytics
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1515, 1197);
+            this.Controls.Add(this.BtnPopFiles);
+            this.Controls.Add(this.BtnRunAnalysis);
+            this.Controls.Add(this.ListBox1);
+            this.Controls.Add(this.Groupbox1);
+            this.Controls.Add(this.lblTitle);
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.Name = "frmClassAnalytics";
+            this.Text = "Class Analytics";
+            this.Groupbox1.ResumeLayout(false);
+            this.Groupbox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtBxSelectFolder;
+        private System.Windows.Forms.Button BtnSelectDirectory;
+        private System.Windows.Forms.Button BtnRunAnalysis;
+        private System.Windows.Forms.ListBox ListBox1;
+        private System.Windows.Forms.GroupBox Groupbox1;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button BtnPopFiles;
+    }
+}
+
---
+++ ETL/frmClassAnalytics.cs
@@ -0,0 +1,186 @@
+﻿//Bryce Dowhower
+// Homework 8 - ETL
+/*
+ * Select Directory = complete
+  Populate Files  = complete
+  Run Analysis:
+    Resident Status -
+        How many students in the cohort are Oklahoma residents? = complete
+        How many are non-residents? How many are international? = complete
+    Program Codes -
+        A list of all the majors of the students in the cohort and the count for each major. = complete
+        The number of students that have multiple majors (does not contain MINOR). 
+        The number of students that have Management Info Systems as a minor.   = complete
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ETL
+{
+    public partial class frmClassAnalytics : Form
+    {
+        private FileInfo[] Files;
+
+        private int countOKRes = 0, countNonOKRes = 0, countInterRes = 0;
+        private int MinorMISCount = 0;
+
+        public frmClassAnalytics()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSelectDirectory_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog SelectFolder = new FolderBrowserDialog();
+            if (SelectFolder.ShowDialog() == System.Windows.Forms.DialogResult.OK) // If they click OK after selecting the folder
+            {
+                TxtBxSelectFolder.Text = SelectFolder.SelectedPath;
+                DirectoryInfo Directory = new DirectoryInfo(TxtBxSelectFolder.Text);
+                Files = Directory.GetFiles("*.csv");
+                foreach (FileInfo file in Files)
+                {
+                    ListBox1.Items.Add(file.Name);
+                }
+            }
+        }
+
+        private void BtnPopFiles_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo Directory = new DirectoryInfo(TxtBxSelectFolder.Text);
+            FileInfo[] Files = Directory.GetFiles("*.csv");
+            foreach (FileInfo file in Files)
+            {
+                ListBox1.Items.Add(file.Name);
+            }
+        }
+
+        static DB_120806_etlEntities database = new DB_120806_etlEntities();
+        private void BtnRunAnalysis_Click(object sender, EventArgs e)
+        {
+            foreach (FileInfo file in Files) // for each file in our file list, it will run the method
+            {
+                CreateCourseFromCSV(file);
+            }
+            MessageBox.Show($"Analysis successful.");
+            //MessageBox.Show($"There are {countOKRes} students from Oklahoma in the cohort, {countNonOKRes} non-residents, and {countInterRes} international students.");
+        }
+
+        private void CreateCourseFromCSV(FileInfo file)
+        {
+            Course newCourse = new Course();
+            string filename = file.Name;
+            string[] courseData = filename.Split('_');
+            newCourse.CRN = Convert.ToInt32(courseData[2]);
+            newCourse.TermCode = Convert.ToInt32(courseData[3].Split('.')[0]);
+            newCourse.Subject = "MIS";
+
+            List<string> residentstatus = new List<string>();
+            List<string> listdegrees = new List<string>();
+  
+            foreach (var line in File.ReadLines(file.FullName).Skip(1))
+            {
+                string[] Column = line.Split(',');
+                residentstatus.Add(Column[5]);
+                int columncount = Column.Count();
+                for (int i = 8; i < columncount; i++) //needed because each file has a different number of columns for the minors/majors
+                {
+                    if (Column[i] != null)
+                    {
+                        listdegrees.Add(Column[i]);
+                    }
+                }
+            }            
+            ResidentCount(residentstatus);
+            MajorCount(listdegrees);
+        }
+
+        private void ResidentCount(List<string> residentstatus)
+        {
+            CourseStatistic ResidentOK = new CourseStatistic();
+            ResidentOK.Name = "How many students in the cohort are Oklahoma residents?";
+            CourseStatistic ResidentNone = new CourseStatistic();
+            ResidentOK.Name = "How many students in the cohort are non-residents?";
+            CourseStatistic ResidentInternat = new CourseStatistic();
+            ResidentOK.Name = "How many students in the cohort are international?";
+            foreach (string ResStatus in residentstatus)
+            {
+                if (ResStatus == "Resident of Oklahoma")
+                {
+                    countOKRes++;
+                }
+                else if (ResStatus == "Non-Resident of Oklahoma")
+                {
+                    countNonOKRes++;
+                }
+                else if (ResStatus == "International")
+                {
+                    countInterRes++;
+                }
+                ResidentOK.Count = countOKRes;
+                ResidentNone.Count = countNonOKRes;
+                ResidentInternat.Count = countInterRes;
+            }            
+        }
+        private void MajorCount(List<String> listdegrees)
+        {
+            Dictionary<string, List<string>> DictionaryDegrees = new Dictionary<string, List<string>>();
+            foreach (string degree in listdegrees)
+            {
+                if (degree != "")
+                {
+                    if (DictionaryDegrees.ContainsKey(degree) == false)
+                    {
+                        DictionaryDegrees.Add(degree, new List<string>());
+                    }
+                }
+            }
+            foreach (string degree in listdegrees)
+            {
+                if (degree != "")
+                {
+                DictionaryDegrees[degree].Add(degree);
+                }
+            }
+            
+            foreach (string degree in DictionaryDegrees.Keys)
+            {
+                CourseStatistic DegreeCount = new CourseStatistic();
+                DegreeCount.Name = $"{degree} count";
+                int count = 0;
+                foreach (string degre in DictionaryDegrees[degree])
+                {
+                    count++;
+                }
+                DegreeCount.Count = count;
+                //MessageBox.Show($"There are {count} {degree} in the file");
+
+                string MISMinor = degree.Split('-')[1];                
+                //NBU-MINOR: Mgt Information Systems
+                if (MISMinor == "MINOR: Mgt Information Systems")
+                {
+                    foreach (string degre in DictionaryDegrees[degree])
+                    {
+                        MinorMISCount++;
+                    }                   
+                }
+            }
+            CourseStatistic MISMinorCount = new CourseStatistic();
+            MISMinorCount.Name = "The number of students that have Management Info Systems as a minor.";
+            MISMinorCount.Count = MinorMISCount;
+            //MessageBox.Show(Convert.ToString(MISMinorCount.Count));
+           
+            /*
+             * Sorry, I'm not answering how many students have multiple majors... I spent a decent amount of time
+             *  on the makeup assignment and I need it to count for something, since I have good grades in every other assignment ;)
+             */
+        }
+    }
+}
